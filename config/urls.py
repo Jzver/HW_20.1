0fb04ef8586708b98beeb9b2961885bc3d0078f1
Rf <jonnyzver78@gmail.com>
2024-06-04 23:35:45 +1000

    исправление ошибок

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,3 @@ urlpatterns = [
     path('', include('catalog.urls', namespace='catalog')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
